ba1ed4a39b2c14cb872230d2f0c3760006f2a108
muhammadfaizan680 <faizanfateh2012@gmail.com>
2025-11-22 18:00:09 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+SQL_CONN_STR = (
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=localhost;'
+    'DATABASE=BanggoodDB;'
+    'Trusted_Connection=yes;'
+)
+
+CATEGORIES = {
+    "smartphones": "https://www.banggood.com/Wholesale-Smartphones-c-1533.html?page=",
+    "laptops": "https://www.banggood.com/Wholesale-Tablet-PC-c-108.html?page=",
+    "smartwatches": "https://www.banggood.com/Wholesale-Smart-Watches-c-5705.html?page=",
+    "rc_toys": "https://www.banggood.com/Wholesale-RC-toys-c-133.html?page=",
+    "home_appliances": "https://www.banggood.com/Wholesale-Home-Appliances-c-1577.html?page="
+}
